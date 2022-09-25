089d8e799ec6dd0526f4cb6f0b11573ba18d85a3
KambalaFish <justdoitnikey@gmail.com>
2022-09-25 13:33:21 +0700

    Some layout changes related to mobile side menu

--- components/NavigationBar/MobileMenu/SideMenu.tsx
+++ components/NavigationBar/MobileMenu/SideMenu.tsx
@@ -32,8 +32,8 @@ const SideMenu = ({
     <Container
       display={['flex', 'flex', 'none']}
       position={'fixed'}
-      left={['0.25rem', '0.75rem']}
-      top={['3rem', '2rem']}
+      left={['0.25rem', '0.5rem']}
+      top={'3rem'}
       zIndex={'2'}
       rowGap={'1.5rem'}
     >

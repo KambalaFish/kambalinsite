bac482baddd7ebcd89cf98d9c6457cdbeaab3aca
KambalaFish <justdoitnikey@gmail.com>
2022-10-07 15:30:55 +0700

    Some work over projects page

---
+++ components/projectsPage/StyledLink.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  color: aqua;
+  text-decoration: none;
+  position: relative;
+  cursor: pointer;
+  font-weight: 700;
+  display: inline-block;
+  transition: transform 0.3s linear;
+  opacity: 0.7;
+  &:hover {
+    transform: translateY(-0.05rem);
+    opacity: 1;
+    transition: transform 0.4s ease-out, opacity 0.4s ease-out;
+  }
+  :hover::after {
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.4s ease-out, visibility ease-out 0.4s;
+  }
+  ::after {
+    position: absolute;
+    content: '';
+    top: 1.75rem;
+    left: 0;
+    right: 0;
+    height: 0.125rem;
+    border-radius: 25%;
+    background-color: ${({ theme }) => theme.fontColor};
+
+    visibility: visible;
+    opacity: 1;
+    transition: opacity 0.3s linear, visibility 0.3s;
+  }
+`;
+
+export { StyledLink };
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,77 +1,62 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { Container, ModalWindow, Text, Title } from '@components';
-import styled from 'styled-components';
+import { Container, ModalWindow, Text, Title, Carousel } from '@components';
 import Image from 'next/future/image';
-import zavod from '/public/projects/zavod2.png';
 import React, { useState } from 'react';
-
-const StyledLink = styled.a`
-  color: aqua;
-  text-decoration: none;
-  position: relative;
-  cursor: pointer;
-  font-weight: 700;
-  display: inline-block;
-  transition: transform 0.3s linear;
-  opacity: 0.7;
-  &:hover {
-    transform: translateY(-0.05rem);
-    opacity: 1;
-    transition: transform 0.4s ease-out, opacity 0.4s ease-out;
-  }
-  :hover::after {
-    opacity: 0;
-    visibility: hidden;
-    transition: opacity 0.4s ease-out, visibility ease-out 0.4s;
-  }
-  ::after {
-    position: absolute;
-    content: '';
-    top: 1.75rem;
-    left: 0;
-    right: 0;
-    height: 0.125rem;
-    border-radius: 25%;
-    background-color: ${({ theme }) => theme.fontColor};
-
-    visibility: visible;
-    opacity: 1;
-    transition: opacity 0.3s linear, visibility 0.3s;
-  }
-`;
-
-const zavodModalChildren = () => {
-  return (
-    <Image
-      src={zavod}
-      quality={100}
-      css={`
-        object-fit: contain;
-        width: 100%;
-        height: auto;
-        //border-radius: 5%;
-      `}
-    />
-  );
-};
+import { css } from 'styled-components';
+import { StyledLink } from '@components/projectsPage';
 
 const Projects: NextPage = () => {
-  const [isModalOpen, setModalVisibility] = useState<boolean>(false);
-  const [modalChildren, setModalChildren] = useState<React.ReactElement>(<></>);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [curIndexZavodProject, setCurIndexZavodProject] = useState(0);
+  const [isZavodModalChildOpen, setIsZavodModalChildOpen] = useState(false);
+
+  const ZavodProjectModalChild = () => (
+    <Carousel
+      curIndex={curIndexZavodProject}
+      setCurIndex={setCurIndexZavodProject}
+      width={['23rem', '32rem', '52.5rem', '55rem', '70rem']}
+      height={['25rem', '32rem', '30rem', '35rem', '40rem']}
+    >
+      <Image
+        src={'/projects/slide-1.png'}
+        css={`
+          object-fit: cover;
+        `}
+        alt={'zavod1'}
+        quality={100}
+        sizes={'100vw'}
+        fill
+      />
+      <Image
+        src={'/projects/slide-2.webp'}
+        css={`
+          object-fit: cover;
+        `}
+        alt={'zavod2'}
+        quality={100}
+        sizes={'100vw'}
+        fill
+      />
+      <Image
+        src={'/projects/slide-3.png'}
+        css={`
+          object-fit: cover;
+        `}
+        alt={'zavod3'}
+        quality={100}
+        sizes={'100vw'}
+        fill
+      />
+    </Carousel>
+  );
 
   return (
     <>
       <Head>
         <title>Проекты</title>
       </Head>
-      <Container
-        minHeight={
-          // 'calc(100vh - 2rem - 3rem - 1rem - 0.625rem*2 - calc(0.3125vw + 10px)*1.2 - 3rem - 6.25rem)'
-          '100vh'
-        }
-        maxWidth={'100%'}
-      >
+      <Container maxWidth={'100%'}>
         <Container>
           <Title>Проекты</Title>
           <Text width={['30ch', '40ch', 'auto']}>
@@ -82,16 +67,14 @@ const Projects: NextPage = () => {
             .
           </Text>
         </Container>
-        <Container width={'30rem'}>
-          <Image
-            src={zavod}
-            quality={100}
-            alt={'zavod'}
-            css={`
-              object-fit: cover;
-              width: 100%;
-              height: calc(30rem / 16 * 11);
-              border-radius: 5%;
+        <Container width={'100%'}>
+          <Carousel
+            curIndex={curIndexZavodProject}
+            setCurIndex={setCurIndexZavodProject}
+            width={'300px'}
+            height={'300px'}
+            stylesForCarouselContainer={css`
+              border-radius: 10%;
               cursor: pointer;
               :hover {
                 scale: 1.01;
@@ -99,18 +82,54 @@ const Projects: NextPage = () => {
               }
               transition: scale 0.4s linear;
             `}
-            onClick={() => {
-              setModalVisibility(true);
-              setModalChildren(zavodModalChildren);
+            onSlideClick={() => {
+              setModalOpen(true);
+              setIsZavodModalChildOpen(true);
             }}
-          />
+          >
+            <Image
+              src={'/projects/slide-1.png'}
+              css={`
+                object-fit: cover;
+              `}
+              alt={'zavod1'}
+              quality={100}
+              sizes={'100vw'}
+              fill
+            />
+            <Image
+              src={'/projects/slide-2.webp'}
+              css={`
+                object-fit: cover;
+              `}
+              alt={'zavod2'}
+              quality={100}
+              sizes={'100vw'}
+              fill
+            />
+            <Image
+              src={'/projects/slide-3.png'}
+              css={`
+                object-fit: cover;
+              `}
+              alt={'zavod3'}
+              quality={100}
+              sizes={'100vw'}
+              fill
+            />
+          </Carousel>
         </Container>
         <ModalWindow
           isVisible={isModalOpen}
-          setModalVisibility={setModalVisibility}
+          setModalVisibility={setModalOpen}
           contentAreaWidth={['100%', '100%', '95%', '80%']}
+          onCloseCb={() => {
+            if (isZavodModalChildOpen) {
+              setIsZavodModalChildOpen(false);
+            }
+          }}
         >
-          {modalChildren}
+          {isZavodModalChildOpen && <ZavodProjectModalChild />}
         </ModalWindow>
       </Container>
     </>

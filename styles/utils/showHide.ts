aed03f7ad7ae50b65af867b521721e12039a9fc6
KambalaFish <justdoitnikey@gmail.com>
2022-10-11 22:08:43 +0700

    showHideTransition function was created

--- styles/utils/showHide.ts
+++ styles/utils/showHide.ts
@@ -1,4 +1,12 @@
+import { css } from 'styled-components';
+
 const showHide = (isVisible: boolean): string =>
   isVisible ? `visibility: visible; opacity: 1;` : `visibility: hidden; opacity: 0;`;
 
-export { showHide };
+type TransitionFn = 'ease-in' | 'ease-out' | 'ease-in-out' | 'linear';
+
+const showHideTransition = (timeMs: number, transitionFn: TransitionFn = 'linear') => css`
+  transition: visibility ${timeMs}ms, opacity ${timeMs}ms ${transitionFn};
+`;
+
+export { showHide, showHideTransition };

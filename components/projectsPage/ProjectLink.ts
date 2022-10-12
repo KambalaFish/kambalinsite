cad53ba7deb6052a4284dd3791e1b1a2c6b6f4bb
KambalaFish <justdoitnikey@gmail.com>
2022-10-12 14:09:58 +0700

    ProjectLink was created

---
+++ components/projectsPage/ProjectLink.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const ProjectLink = styled.a`
+  display: inline-block;
+  cursor: pointer;
+  padding: 0.5rem 1.25rem;
+  background-image: linear-gradient(to left, salmon 35%, lightblue 65%);
+  background-position: right;
+  background-size: 300% 100%;
+  transition: background-position 0.6s ease-out, color 0.6s ease-out, box-shadow 0.6s ease-out;
+  border-radius: 10% / 50%;
+  overflow: hidden;
+  box-shadow: 0 0 0.25rem 0 rgb(255, 255, 255);
+  color: rgb(32, 32, 32);
+  font-weight: 800;
+  :hover {
+    color: rgb(0, 0, 0);
+    background-position: left;
+    box-shadow: inset 0 0 0.5rem 0 rgb(0, 0, 0);
+    transition: background-position 0.4s ease-in, color 0.4s ease-in, box-shadow 0.4s ease-in;
+  }
+`;
+
+export { ProjectLink };

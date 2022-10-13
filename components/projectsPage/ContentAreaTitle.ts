ef83ae3e3a4dcc62942b81c67f62adacb99ef2a7
KambalaFish <justdoitnikey@gmail.com>
2022-10-13 15:57:56 +0700

    Some work over zavod card content

---
+++ components/projectsPage/ContentAreaTitle.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Title } from '@components';
+
+const ContentAreaTitle = styled(Title)`
+  font-size: 2rem;
+  margin: 0;
+`;
+
+export { ContentAreaTitle };

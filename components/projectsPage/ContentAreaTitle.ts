ba328cec1947d3eb1e2da7018627d33ef796c4cf
KambalaFish <justdoitnikey@gmail.com>
2022-10-15 13:28:58 +0700

    ContentAreaTitle was modified

--- components/projectsPage/ContentAreaTitle.ts
+++ components/projectsPage/ContentAreaTitle.ts
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 import { Title } from '@components';
+import { minDeviceMedia } from '@styles/theme';
 
 const ContentAreaTitle = styled(Title)`
-  font-size: 2rem;
-  margin: 0;
+  font-size: 1.75rem;
+  margin: 0.5rem 0 0.25rem;
+  ${minDeviceMedia.mobile} {
+    font-size: 2rem;
+  }
+  ${minDeviceMedia.tablet} {
+    font-size: 2.5rem;
+    margin: 0.75rem 0 0.5rem;
+  }
 `;
 
 export { ContentAreaTitle };

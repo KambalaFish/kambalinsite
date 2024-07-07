a8c334d8b17420854eb034162498150408370557
KambalaFish <justdoitnikey@gmail.com>
2024-07-07 22:22:31 +0300

    Theme policy was changed to avoid blink

--- hooks/useTheme.ts
+++ hooks/useTheme.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { darkTheme, lightTheme } from '@styles/theme';
 import { DefaultTheme } from 'styled-components';
 
@@ -14,47 +14,15 @@ export type ThemeHook = {
 };
 
 function useTheme(): ThemeHook {
-  const [themeMode, setThemeMode] = useState<ThemeMode>(ThemeMode.light);
+  const [themeMode, setThemeMode] = useState<ThemeMode>(ThemeMode.dark);
 
-  function saveThemeMode(theme: ThemeMode) {
-    setThemeMode(theme);
-    window.localStorage.setItem('theme', theme);
-  }
-
-  const themeToggle = useCallback(() => {
-    themeMode === ThemeMode.light ? saveThemeMode(ThemeMode.dark) : saveThemeMode(ThemeMode.light);
-  }, [themeMode]);
-
-  useEffect(() => {
-    const savedTheme = window.localStorage.getItem('theme') as ThemeMode | undefined;
-    /*правило такое:
-    если пользователь ранее менял тему (тыкая кнопку), то ставим её как изначальную при повторном посещении страницы.
-    если пользователь ни разу не выбирал тему (не тыкал кнопочку), то используем его системную тему*/
-    if (savedTheme) {
-      setThemeMode(savedTheme);
-    } else if (window.matchMedia) {
-      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        setThemeMode(ThemeMode.dark);
-      } else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-        setThemeMode(ThemeMode.light);
-      }
-    }
-
-    const colorSchemeListener = (event: MediaQueryListEvent): void => {
-      const newColorScheme = event.matches ? ThemeMode.dark : ThemeMode.light;
-      setThemeMode(newColorScheme);
-    };
-
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', colorSchemeListener);
-
-    return () => {
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .removeEventListener('change', colorSchemeListener);
-    };
-  }, []);
+  const themeToggle = useCallback(
+    () =>
+      setThemeMode((themeMode) =>
+        themeMode === ThemeMode.light ? ThemeMode.dark : ThemeMode.light
+      ),
+    []
+  );
 
   return {
     theme: themeMode === ThemeMode.light ? lightTheme : darkTheme,
